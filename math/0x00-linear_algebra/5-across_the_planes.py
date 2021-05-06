6c7905a042b9429d6d0edbb144db59594de32618
JDorangetree <julian.naranjo2014gmail.com>
2021-05-06 03:17:27 +0000

    pycodestyle

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,12 +4,14 @@
 
 def add_matrices2D(mat1, mat2):
     """return new matrix"""
-    element = [[1, 2], [1, 2]]
-    if len(mat1[0]) == len(mat2[0]):
+    element = [[1, 1], [1, 1]]
+    if len(mat1) != len(mat2):
+        return(None)
+    elif len(mat1[0]) != len(mat2[0]):
+        return(None)
+    else:
         for i in range(0, len(mat1)):
             for j in range(0, len(mat1[0])):
                 item = mat1[i][j] + mat2[i][j]
                 element[i][j] = item
-    else:
-        return(None)
-    return(element)
+        return(element)
