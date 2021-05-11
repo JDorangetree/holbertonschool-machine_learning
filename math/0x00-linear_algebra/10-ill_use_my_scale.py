f99c17cc0605aca4436c11249c4b63bf187f4fd9
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-10 21:31:51 -0500

    task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """return tuple"""
+    shape = matrix.shape
+    return(shape)
