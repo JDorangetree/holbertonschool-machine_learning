3f07f229f93a770cca371f69a0e9ba3648f66a89
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-11 09:23:19 -0500

    task 13

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
+import numpy as np
 """function that concatenates two matrices along a specific axis"""
 
 
 def np_cat(mat1, mat2, axis=0):
-    import numpy as np
     """return numpy array"""
     concat = np.concatenate((mat1, mat2), axis=axis)
     return(concat)
