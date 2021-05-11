47bb060d75c4c105682bb9b26661e125363f7ca6
JDorangetree <julian.naranjo2014gmail.com>
2021-05-11 00:44:55 +0000

    prueba

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def add_matrices2D(mat1, mat2):
     if len(mat1[0]) != len(mat2[0]):
         return(None)
     else:
-        element = [[1,1],[1,1]]
+        element = [[1, 1], [1, 1]]
         for i in range(0, len(mat1)):
             for j in range(0, len(mat1[0])):
                 item = mat1[i][j] + mat2[i][j]
