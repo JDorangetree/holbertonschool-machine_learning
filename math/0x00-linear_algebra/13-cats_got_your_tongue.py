11d83faf757ae036f73cbe32d853317c24a742c9
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-11 09:31:46 -0500

    task 13

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+"""function that concatenates two matrices along a specific axis"""
 
 
 import numpy as np
-
-
-"""function that concatenates two matrices along a specific axis"""
 
 
 def np_cat(mat1, mat2, axis=0):
