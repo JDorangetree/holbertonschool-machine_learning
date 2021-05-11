591da65496c8f4f6b11044e6a473491ecb0c577a
JDorangetree <julian.naranjo2014gmail.com>
2021-05-11 00:53:49 +0000

    prueba

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -6,6 +6,10 @@ def add_matrices2D(mat1, mat2):
     """return new matrix"""
     if len(mat1) != len(mat2):
         return(None)
+    if len(mat1[0]) != len(mat2[0]):
+        return(None)
+    if len(mat1[0]) != len(mat1[1]):
+        return(None)
     else:
         element = [[1, 1], [1, 1]]
         for i in range(0, len(mat1)):
