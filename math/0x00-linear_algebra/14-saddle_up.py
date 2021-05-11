3faed5f190e92d9ab96b493a97602e780993aff1
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-11 09:42:07 -0500

    task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function that performs matrix multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """return numpy.ndarray"""
+    matmul = np.matmul(mat1, mat2)
+    return(matmul)
