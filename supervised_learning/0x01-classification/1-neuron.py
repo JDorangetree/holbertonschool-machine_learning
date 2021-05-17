99fe973356987b6f7d8684f6eee66398676a8cc4
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-16 19:40:45 -0500

    task 1

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""class that defines a single neuron performing binary classification"""
+
+
+import numpy as np
+
+
+class Neuron():
+    """class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize method"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.randn(nx).reshape((1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Returns: private instance weight
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Returns: private instance bias
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Returns: private instance output
+        """
+        return self.__A
