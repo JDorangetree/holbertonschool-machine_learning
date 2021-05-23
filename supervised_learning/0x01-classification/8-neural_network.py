0a8e2b6cd961895a9b02851b15654a8714f97aef
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-23 15:39:41 -0500

    taks 8

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""class that defines a neural network with one hidden layer"""
+
+
+import numpy as np
+
+
+class NeuralNetwork():
+    """class that defines a single neuron performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """Initialize method"""
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) != int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.randn(nx).reshape((1, nx))
+        self.b1 = 0
+        self.A1 = 0
+        self.W2 = np.random.randn(nx).reshape((1, nx))
+        self.b2 = 0
+        self.A2 = 0
