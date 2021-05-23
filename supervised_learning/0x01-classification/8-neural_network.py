7cdf2805bb097dca866fa3d8bdd5af7d12843f61
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-23 15:45:18 -0500

    task 8

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -12,16 +12,16 @@ class NeuralNetwork():
         """Initialize method"""
         if type(nx) != int:
             raise TypeError('nx must be an integer')
-        if nx < 1:
+        elif nx < 1:
             raise ValueError('nx must be a positive integer')
-        if type(nodes) != int:
+        elif type(nodes) != int:
             raise TypeError('nodes must be an integer')
-        if nodes < 1:
+        elif nodes < 1:
             raise ValueError('nodes must be a positive integer')
 
         self.W1 = np.random.randn(nx).reshape((1, nx))
         self.b1 = 0
         self.A1 = 0
-        self.W2 = np.random.randn(nx).reshape((1, nx))
+        self.W2 = np.random.randn(nx).reshape((1, nodes))
         self.b2 = 0
         self.A2 = 0
