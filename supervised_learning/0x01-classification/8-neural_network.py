0fd91627becc44abb3525b629e189a01f22914c7
Julian Naranjo <julian.naranjo2014@gmail.com>
2021-05-23 15:46:45 -0500

    task 8

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -22,6 +22,6 @@ class NeuralNetwork():
         self.W1 = np.random.randn(nx).reshape((1, nx))
         self.b1 = 0
         self.A1 = 0
-        self.W2 = np.random.randn(nx).reshape((1, nodes))
+        self.W2 = np.random.randn(nodes).reshape((1, nodes))
         self.b2 = 0
         self.A2 = 0
