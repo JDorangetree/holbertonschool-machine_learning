1cf564b7b4438bde07a02cc959ca1f7f21e005af
JDorangetree <julian.naranjo2014@gmail.com>
2021-05-26 21:03:04 -0500

    task 0

---
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Script to use L2 regularization in a DNN"""
+
+import numpy as np
+from numpy.core.numeric import normalize_axis_tuple
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """calculates the cost of a neural network with L2 regularization"""
+    keys = list(weights.keys())
+    normalize = 0
+    for i in keys:
+        if i[0] == 'W':
+            normalize = normalize + np.linalg.norm(weights[i])
+    L2 = cost + (lambtha/(2 * m)) * normalize
+    return(L2)
