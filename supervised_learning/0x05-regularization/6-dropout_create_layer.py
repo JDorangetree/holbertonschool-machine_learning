90d6278b0ef49e529fd4d4873d287e85a41a2b26
JDorangetree <julian.naranjo2014@gmail.com>
2021-05-27 21:34:44 -0500

    task 6

---
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Script to implement dropout in tensorflow"""
+
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """Function that uses dropout in tensorflow"""
+    do = tf.layers.Dropout(keep_prob)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    tensor = tf.layers.Dense(units=n, activation=activation,
+                             kernel_initializer=init,
+                             kernel_regularizer=do)
+    return tensor(prev)
