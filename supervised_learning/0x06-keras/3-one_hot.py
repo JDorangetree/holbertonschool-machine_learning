430ab965d9101fc6128f6754daea1bbaf8043288
JDorangetree <julian.naranjo2014@gmail.com>
2021-05-31 11:40:51 -0500

    task 3

---
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""builds a neural network with the Keras library"""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    one_hot = K.utils.to_categorical(labels, num_classes=len(labels))
+    return(one_hot)
