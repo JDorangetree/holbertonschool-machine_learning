0d5b4c40240d84f47f9700c06e10442fffec68f4
JDorangetree <julian.naranjo2014@gmail.com>
2021-06-03 17:25:56 -0500

    task 4

---
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Script to perform a convolution of images with channels"""
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Function to perform a convolution of img with channels"""
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    c = images.shape[3]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    pad_w = 0
+    pad_h = 0
+    sh = stride[0]
+    sw = stride[1]
+
+    if padding == 'same':
+        pad_h = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pad_w = int((((w - 1) * sw + kw - w) / 2) + 1)
+    if type(padding) == tuple:
+        pad_h = padding[0]
+        pad_w = padding[1]
+
+    image_pad = np.pad(images, pad_width=((0, 0), (pad_h, pad_h),
+                                          (pad_w, pad_w), (0, 0)),
+                       mode='constant')
+
+    output_h = int(((h + 2 * pad_h - kh) / sh) + 1)
+    output_w = int(((w + 2 * pad_w - kw) / sw) + 1)
+
+    out = np.zeros((m, output_h, output_w))
+
+    image = np.arange(m)
+    for i in range(output_h):
+        for j in range(output_w):
+            out[image, i, j] = (np.sum(image_pad[image,
+                                i * sh:((i * sh) + kh),
+                                            j * sw:((j * sw) + kw)] * kernel,
+                                axis=(1, 2, 3)))
+    return out
