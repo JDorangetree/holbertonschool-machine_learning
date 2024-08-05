80dd9a73f1751f3c0e0b641676d35d4d1302918e
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 14:22:40 -0500

    6 7 8

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""function that adds two matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """return new matrix"""
+    if len(mat1) != len(mat2):
+        return None
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
+        element = [[1, 1], [1, 1]]
+        for i in range(0, len(mat1)):
+            for j in range(0, len(mat1[0])):
+                item = mat1[i][j] + mat2[i][j]
+                element[i][j] = item
+    return element
---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """return array"""
+    arr3 = arr1 + arr2
+    return arr3
