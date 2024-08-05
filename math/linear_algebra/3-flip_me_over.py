c5c50e6116a4019c490fe8a362bb9a31596db529
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 14:12:15 -0500

    3 y 4

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function that returns the transpose of a 2D matrix"""
+
+
+def matrix_transpose(element):
+    """return a new matrix"""
+    new_matrix = []
+    for i in range(0, len(element[0])):
+        count = 0
+        row = []
+        while count < len(element):
+            row.append(element[count][i])
+            count += 1
+        new_matrix.append(row)
+    return(new_matrix)
---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that adds two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """return new list"""
+    result = []
+    if len(arr1) == len(arr2):
+        for i in range(0, len(arr1)):
+            result.append(arr1[i] + arr2[i])
+        return(result)
+    else:
+        return(None)
