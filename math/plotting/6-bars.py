97770b14ad2935c364a488337a3388f4eeb9e899
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 18:54:25 -0500

    6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -11,7 +11,7 @@ def bars():
     labels = ['Farrah', 'Fred', 'Felicia']
     width = 0.35
     l_fruit = ['apples', 'bananas', 'oranges', 'peaches']
-    c = ['r', 'y', '#FF8000', '#FFE5b4']
+    c = ['r', 'yellow', '#FF8000', '#FFE5b4']
 
     y_offset = np.zeros(len(labels))
 
@@ -25,3 +25,4 @@ def bars():
     plt.legend()
 
     plt.show()
+bars()
