77c70fd15e6e177b826e24f04546569ce3ffb53e
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 18:49:38 -0500

    6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,23 +2,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
-labels = ['Farrah', 'Fred', 'Felicia']
-width = 0.35
-l_fruit = ['apples', 'bananas', 'oranges', 'peaches']
-c = ['r', '#FFFF00', '#FF8000', '#FFE5b4']
 
-y_offset = np.zeros(len(labels))
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
 
-for row in range(len(fruit)):
-    plt.bar(labels, fruit[row], bottom=y_offset,
-            label=l_fruit[row], color=c[row])
-    y_offset = y_offset + fruit[row]
+    labels = ['Farrah', 'Fred', 'Felicia']
+    width = 0.35
+    l_fruit = ['apples', 'bananas', 'oranges', 'peaches']
+    c = ['r', '#FFFF00', '#FF8000', '#FFE5b4']
 
+    y_offset = np.zeros(len(labels))
 
-plt.ylabel('Quantity of Fruit')
-plt.title('Number of Fruit per Person')
-plt.legend()
+    for row in range(len(fruit)):
+        plt.bar(labels, fruit[row], bottom=y_offset,
+                label=l_fruit[row], color=c[row])
+        y_offset = y_offset + fruit[row]
 
-plt.show()
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+
+    plt.show()
