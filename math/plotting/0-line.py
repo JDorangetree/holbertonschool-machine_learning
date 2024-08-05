39546a119da321f030b7845d0c5dac22182544ca
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 18:09:08 -0500

    0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,8 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
 
-plt.xlim(0, 10)
-plt.plot(y, color='r')
-plt.show()
+def line():
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 10)
+    plt.plot(y, color='r')
+    plt.show()
