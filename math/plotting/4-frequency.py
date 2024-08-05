109a33bb75f6083174ff1303d527387f26e46e27
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 18:22:10 -0500

    4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+"""function that show a line"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades, 10, facecolor='#228bc2', edgecolor='black')
-plt.title('Project A')
-plt.xlabel("Grades")
-plt.ylabel("Number of Students")
-plt.show()
+def frequency():
+    """plt show"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, 10, facecolor='#228bc2', edgecolor='black')
+    plt.title('Project A')
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.show()
