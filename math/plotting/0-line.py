db00c591d83c66124888990d699e51519f320583
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 18:13:19 -0500

    0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""function that show a line"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """plt show"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.xlim(0, 10)
