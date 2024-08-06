8d2bf8f088038c30512c25cf1537ca9ca8ad9c84
JDorangetree <orangetree@LAPTOP-COKI4LG3>
2024-08-05 19:06:41 -0500

    6

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -2,17 +2,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
 
-x = np.random.randn(2000) * 10
-y = np.random.randn(2000) * 10
-z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+def gradient():
 
-# your code here
+    np.random.seed(5)
 
-plt.title("Mountain Elevation")
-plt.xlabel("x coordinate (m)")
-plt.ylabel("y coordinate (m)")
-plt.scatter(x, y, s=40, c=z)
-plt.colorbar(label="elevation (m)")
-plt.show()
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.title("Mountain Elevation")
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
+    plt.scatter(x, y, s=40, c=z)
+    plt.colorbar(label="elevation (m)")
+    plt.show()
